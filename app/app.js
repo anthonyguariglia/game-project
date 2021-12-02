42e4c30e801229749568cd7a470e180b3b2d7504
anthonyguariglia <aguariglia197@gmail.com>
2021-12-02 11:33:49 -0500

    Add event listeners for tic tac toe buttons
    Adds 'click' event listeners for the `div`s that represent each tic tac toe square. On each click the respective callback is called to populate that square with its required information.

--- app/app.js
+++ app/app.js
@@ -1,9 +1,19 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
+const gameEvent = require('./game/events.js')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
   // your JS code goes here
+  $('#0-0').on('click', gameEvent.on00Click)
+  $('#0-1').on('click', gameEvent.on01Click)
+  $('#0-2').on('click', gameEvent.on02Click)
+  $('#1-0').on('click', gameEvent.on10Click)
+  $('#1-1').on('click', gameEvent.on11Click)
+  $('#1-2').on('click', gameEvent.on12Click)
+  $('#2-0').on('click', gameEvent.on20Click)
+  $('#2-1').on('click', gameEvent.on21Click)
+  $('#2-2').on('click', gameEvent.on22Click)
 })
