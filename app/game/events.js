9f5fe2b2f7ca078e47bdfbd5366717843aac9281
anthonyguariglia <aguariglia197@gmail.com>
2021-12-03 11:50:22 -0500

    Modify events.js to fix newGame, multiple clicks
    Clears the board when newGame is clicked
    
    Does not allow onClick to perform a function if a space has been clicked for the second time

--- app/game/events.js
+++ app/game/events.js
@@ -4,70 +4,81 @@ const gameUI = require('./ui.js')
 const store = require('../store.js')
 
 let n = 0
-const gameArray =
+let gameArray =
     [['', '', ''],
       ['', '', ''],
       ['', '', '']]
 
 const player = [
   {
+    name: 'Player 1',
     symbol: 'x'
   },
   {
+    name: 'Player 2',
     symbol: 'o'
   }
 ]
 
-let gameWinner
+let gameWinner = [false, '']
 
 const onNewGame = function () {
   gameAPI.newGame()
     .then(gameUI.newGameSuccess)
     .catch(gameUI.newGameFailure)
+  gameArray = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ]
+  $('#0-0, #0-1, #0-2, #1-0, #1-1, #1-2, #2-0, #2-1, #2-2').text('')
 }
 
 const onClick = function (event) {
-  event.preventDefault()
-  // Pull 2D cell location from ID
-  const positionIn2DArray = event.target.id
-  console.log(event.target)
-  const row = parseInt(positionIn2DArray.split('-')[0])
-  const col = parseInt(positionIn2DArray.split('-')[1])
-  // get latest game board
-  //   gameAPI.getGameBoard(store.game._id)
-  //     .then(gameUI.onGetBoardSuccess)
-  //     .catch(gameUI.onGetBoardFailure)
-  // Update game array with new value
-  console.log(player[n % 2])
-  console.log(row, col)
-  gameArray[row][col] = player[n % 2].symbol
-  // check for winner
-  gameWinner = evaluate(gameArray)
-  // Compute 1D array cell value for API
-  const cell = 3 * row + col
-  gameAPI.updateGameBoard(cell, player[n % 2].symbol, gameWinner[0], store.game._id)
-    .then(gameUI.onUpdateBoardSuccess)
-    .catch(gameUI.onUpdateBoardFailure)
-  // End game if winner exists
-  if (gameWinner[0]) {
-    endGame()
+  if (!gameWinner[0]) {
+    event.preventDefault()
+    // Pull 2D cell location from ID
+    const positionIn2DArray = event.target.id
+    console.log(event.target)
+    const row = parseInt(positionIn2DArray.split('-')[0])
+    const col = parseInt(positionIn2DArray.split('-')[1])
+    console.log(gameArray[row][col])
+    if (gameArray[row][col] === '') {
+      // Update game array with new player value
+      gameArray[row][col] = player[n % 2].symbol
+      // Update game board on screen
+      $(event.target).text(player[n % 2].symbol)
+
+      // check for winner
+      gameWinner = evaluate()
+      // Compute 1D array cell value for API
+      const cell = 3 * row + col
+      // Update API with new board values
+      gameAPI.updateGameBoard(cell, player[n % 2].symbol, gameWinner[0], store.game._id)
+        .then(gameUI.onUpdateBoardSuccess)
+        .catch(gameUI.onUpdateBoardFailure)
+      if (gameWinner[0]) {
+        endGame(gameWinner)
+      }
+
+      // increment turn
+      nextTurn()
+    }
   }
-  // increment turn
-  nextTurn()
 }
 
 const nextTurn = function () {
   n++
 }
 
-const evaluate = function (player) {
-  let winner = false
+const evaluate = function () {
+  let winner = [false, '']
   for (let row = 0; row < 3; row++) {
     if (gameArray[row][0]) {
       if (
         gameArray[row][0] === gameArray[row][1] && gameArray[row][1] === gameArray[row][2]
       ) {
-        winner = [true, player[n % 2]]
+        winner = [true, player[n % 2].name]
       }
     }
   }
@@ -75,25 +86,25 @@ const evaluate = function (player) {
     if (gameArray[0][col]) {
       if (
         gameArray[0][col] === gameArray[1][col] && gameArray[1][col] === gameArray[2][col]) {
-        winner = [true, player[n % 2]]
+        winner = [true, player[n % 2].name]
       }
     }
   }
   if (gameArray[0][0]) {
     if (gameArray[0][0] === gameArray[1][1] && gameArray[1][1] === gameArray[2][2]) {
-      winner = [true, player[n % 2]]
+      winner = [true, player[n % 2].name]
     }
   }
   if (gameArray[0][2]) {
     if (gameArray[0][2] === gameArray[1][1] && gameArray[1][1] === gameArray[2][0]) {
-      winner = [true, player[n % 2]]
+      winner = [true, player[n % 2].name]
     }
   }
   return winner
 }
 
-const endGame = function () {
-  // add code for end of game
+const endGame = function (winner) {
+  console.log(`Game over! ${winner[1]} wins!`)
 }
 
 module.exports = {
